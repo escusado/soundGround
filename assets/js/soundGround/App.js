6d0ca587662788b11867f1cbe3138c3bd2eec0e8
joaquin benitez <joaquin.benitez@gmail.com>
2013-05-07 20:46:48 -0500

    use neon std

---
+++ assets/js/soundGround/App.js
@@ -0,0 +1,12 @@
+window.soundGround = {
+    start : function(){
+        
+        //get ID
+
+        var app = new Controller({
+            data : window.dummy
+        });
+
+        app.render($('.wrapper'));
+    }
+};
---
+++ assets/js/soundGround/Controller.js
@@ -0,0 +1,18 @@
+Class('Controller').inherits(Widget)({
+
+    ELEMENT_CLASS : 'app-wrapper',
+
+    HTML : '<div> </div>',
+
+    prototype : {
+
+        init : function(config) {
+            Widget.prototype.init.call(this, { config : config });
+
+            
+            
+        }
+
+    }
+
+});
